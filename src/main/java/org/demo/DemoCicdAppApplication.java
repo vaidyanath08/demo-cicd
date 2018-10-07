d0058037afc5323aa39b073055ead9ff6d893ffd
vaidyanath08 <vaidyanathrajpoot@gmail.com>
2018-10-07 12:26:07 +0530

    my first commit

---
+++ src/main/java/org/demo/DemoCicdAppApplication.java
@@ -0,0 +1,12 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdAppApplication.class, args);
+	}
+}
